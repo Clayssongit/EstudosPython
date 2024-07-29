fd3374e737b06255f92e3d3deb8ba97d80bda0ed
Claysson Eugenio <clayssongit@hotmail.com>
2024-07-29 12:25:25 -0300

    Create 10-listas.py

---
+++ PYTHONFUND/10-listas.py
@@ -0,0 +1,16 @@
+gameFifa = ["Fifa 23", 2022, 90.50, True]
+
+gameList = ["Resident Evil 4", "Star Wars Jedi", 
+            "The Legend of zelda", "Read Dead 2","Mario Odyssey"]
+
+#1 - Buscar os dois primeiros itens da lista
+print(gameList[:2])
+
+#2 - Buscar o último item da lista
+print(gameList[:-1])
+
+#3 - Buscar jogos até uma determinada posição
+print(gameList[:3])
+
+#4 - Buscar jogos de uma posição em diante
+print(gameList[1:4])
