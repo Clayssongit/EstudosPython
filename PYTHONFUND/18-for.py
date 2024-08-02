6904d1458696a563db16284793d93ba52e3fd21d
Claysson Eugenio <clayssongit@hotmail.com>
2024-08-02 17:34:16 -0300

    Create 18-for.py

---
+++ PYTHONFUND/18-for.py
@@ -0,0 +1,27 @@
+gameList = ["Fifa", "God of War", "Red Dead 2", "Uncharted"]
+
+# 1 - Interando valores de uma lista
+for game in gameList:
+    print(game)
+
+# 2 - Quando a condição for atendida, o Loop será encerrado
+for game in gameList:
+    if game == "Red Dead 2":
+        break 
+    print(game)
+
+# 3 - Quando a condição for atendida, o Loop vai para a próxima interação
+for game in gameList:
+    if game == "Red Dead 2":
+        continue
+    print(game)
+
+# 4 - Avaliação do jogo
+gameName = input("Digite o nome do jogo: ")
+gameRating = int(input("Digite quantas avaliações deseja fazer do jogos: "))
+
+sum = 0
+for i in range(gameRating):
+    note = float(input("Digite a nota para o jogo: "))
+    sum += note
+print("Média de avaliação do jogo {} é {}".format(gameName, sum/gameRating))
