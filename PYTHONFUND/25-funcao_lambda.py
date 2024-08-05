6ba252cdabc9dd555cc94b8fa5a2c1b0feac6168
Claysson Eugenio <clayssongit@hotmail.com>
2024-08-05 13:50:48 -0300

    Update 25-funcao_lambda.py

---
+++ PYTHONFUND/25-funcao_lambda.py
@@ -0,0 +1,11 @@
+# 1 - Função de pôtencia de número
+power = lambda num: num **2
+
+# 2 - Função verifica se o número é par
+pair = lambda x: x % 2 == 0
+
+# 3 - Função que divide um número por outro
+divnum = lambda x,y: x/y
+
+# 4 - Função que inverte uma string
+reverse = lambda s: s[::-1]
