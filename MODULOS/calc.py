7e414268dfc2fc10edb5a362215b2a3dbde2918a
Claysson Eugenio <clayssongit@hotmail.com>
2024-08-07 16:57:53 -0300

    Update calc.py

---
+++ MODULOS/calc.py
@@ -0,0 +1,15 @@
+def sum(a, b):
+    result = a + b
+    return
+
+def sub(a, b):
+    result = a - b
+    return
+
+def mult(a, b):
+    result = a * b
+    return
+
+def div(a, b):
+    result = a / b
+    return
