057e815af8904a98ecffbeb2ae813cc7a9be8dcd
Clayssongit <clayssongit@hotmail.com>
2024-08-10 18:08:05 -0300

    Mod Builtin

---
+++ MODULOS/2-modulos.py
@@ -0,0 +1,6 @@
+help('modules')
+
+"""
+Módulos Builtin, são essências para quem útiliza o python e quer se aprofundar cada vez mais
+ para saber mais sobre cada um deles e suas funcionalidades acesse o site https://docs.python.org/3/py-modindex.html
+                                                                                                                """
