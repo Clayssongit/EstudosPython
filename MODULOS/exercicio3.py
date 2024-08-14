b496eadf7b9c542dba8ada9089371ed999889f38
Claysson Eugenio <clayssongit@hotmail.com>
2024-08-14 15:08:18 -0300

    Update exercicio3.py

--- MODULOS/exercicio3.py
+++ MODULOS/exercicio3.py
@@ -8,9 +8,9 @@ A-Z e 0-9).
 """
 
 def check_character(string):
-    rule = re.compile(r'[^a-zA-Z0-9]')
+    rule = re.compile(r'[^a-zA-Z0-9]') # compile possibilita passar mais de uma regra
     string = rule.search(string)
-    return not bool (string)
+    return not bool (string) 
 
 print(check_character('102930123910mksdmfsakfdnaAKNSA'))
 print(check_character('102930123910mksdmf#@$*&!'))
