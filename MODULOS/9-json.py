f13ea77c6b78b7aa5834db827ea4b839d79c8ecb
Claysson Eugenio <clayssongit@hotmail.com>
2024-08-16 12:33:36 -0300

    up-json

--- MODULOS/9-json.py
+++ MODULOS/9-json.py
@@ -16,3 +16,8 @@ print(json.dumps(person_dict, indent=4, sort_keys=True))
 # 4 - Salvando json em txt
 with open("person.txt", "w") as json_file:
     json.dump(person_dict, json_file)
+
+# 5 - Lendo json externo
+with open("iris.json", "r") as f:
+    data = json.load(f)
+    print(data)
