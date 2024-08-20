7481cf0c3d0b683d4ca0ccb177b8a7605cc81d32
Claysson Eugenio <clayssongit@hotmail.com>
2024-08-20 17:49:49 -0300

    Update 12-tkinter.py

--- MODULOS/12-tkinter.py
+++ MODULOS/12-tkinter.py
@@ -13,4 +13,20 @@ frame.pack(padx=10, pady=10, fill= 'x', expand= True)
 label = tk.Label(frame, text="Hello, World")
 label.pack(fill='x', expand=True)
 
+# 4 - Adicionar o input text
+frase_lab = tk.Label(frame, text="Frase")
+frase_lab.pack(fill='x', expand=True)
+
+frase_inp = tk.Entry(frame)
+frase_inp.pack(fill='x', expand=True)
+
+# 5 - Função para alterar o texto do label
+def click():
+    label.config(text=frase_inp.get())
+
+
+# 6 - Adicionando botão
+button = tk.Button(frame, text="Enviar", command=click)
+button.pack()
+
 window.mainloop()
