dbb30e137091a3d8bece47fb94491a000aa9e545
Claysson Eugenio <clayssongit@hotmail.com>
2024-09-27 15:40:08 -0300

    Update 6-class_metodos.py

--- POO/6-class_metodos.py
+++ POO/6-class_metodos.py
@@ -9,6 +9,10 @@ class Console:
         self.name = name
         self.price = price
     
+    def __str__(self):
+        return (f"Console {self.name}, está por R${self.price}.")
+    
+    
     @classmethod
     def from_text(cls, string):
         import re
@@ -19,5 +23,6 @@ class Console:
     
 nitendo = Console.from_text("Meu vídeo game é Nitendo e o preço é 1980 reais")
 ps5 = Console.from_text("Meu outro vídeo game é ps5 e o valor dele é 3500 reais")
+ps3 = Console("ps3", 2000)
 print(nitendo.__dict__)
-print(ps5)
+print(ps3)
