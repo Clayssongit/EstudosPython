bfa3aec017778914ef9dbafc25c499aa64f6190a
Claysson Eugenio <clayssongit@hotmail.com>
2024-10-03 12:26:27 -0300

    Create 9-getter_setter.py

---
+++ POO/9-getter_setter.py
@@ -0,0 +1,22 @@
+class Enployee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.__salary = salary
+    
+    def show(self):
+        print(f"Nome {self.name} - Salário {self.__salary}")
+
+
+#Metodo para buscar dados
+    def get_salary(self):
+        return self.__salary
+#Metodo para modificar atributos privados
+    def set_salary(self, salary):
+        self.__salary = salary
+
+mario = Enployee("Mario", 4000)
+maria = Enployee("Maria", 5000)
+
+mario.show()
+mario.set_salary(7000)
+mario.show()
