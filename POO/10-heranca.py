87f248b89a528be312381b19dc6a83463ba38e38
Claysson Eugenio <clayssongit@hotmail.com>
2024-10-03 15:33:53 -0300

    Create 10-heranca.py

---
+++ POO/10-heranca.py
@@ -0,0 +1,35 @@
+class Animal:
+    name = ""
+    size = ""
+    color = ""
+
+    def eat(self):
+        print("Animal se alimentando")
+
+class Horse(Animal):
+    race = ""
+
+    def escape(self):
+        print("cavalo fugindo")
+
+class Leon(Animal):
+    mane = True
+
+    def hunt(self):
+        print("Leão caçando")
+
+h=Horse()
+h.name="Carpeado"
+h.color="Preto"
+h.escape()
+h.eat()
+print(h.name)
+
+print("")
+
+l=Leon()
+l.name="Simba"
+l.color="Marrom"
+print(l.name)
+l.hunt()
+l.eat()
